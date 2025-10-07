765850cfe3a2b9c5dada396fb5fba258c4694fb1
Kartik <kartik140406@gmail.com>
2025-10-07 07:15:19 +0530

    convex setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
-import {SignedIn, SignedOut, SignOutButton, SignUpButton } from "@clerk/nextjs";
+import {SignedIn, SignedOut, SignOutButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return <div>
     <SignedOut>
-      <SignUpButton/>
-    </SignedOut>
+        <SignUpButton>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Sign Up
+          </button>
+        </SignUpButton>
+      </SignedOut>
 
+    <UserButton />
     <SignedIn>
-      <SignOutButton/>
-    </SignedIn>
+        <div className="flex items-center space-x-4 mt-4">
+          <UserButton />
+          <SignOutButton>
+            <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+              Sign Out
+            </button>
+          </SignOutButton>
+        </div>
+      </SignedIn>
   </div>;
 }
